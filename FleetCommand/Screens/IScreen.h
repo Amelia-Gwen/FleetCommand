5f33cadc2a8d1efb8cd55972385f6784d89477d4
bruglesco <klitzinger@gmail.com>
2019-03-10 22:52:20 -0400

    Added virtual method signatures to IScreen base class.

--- FleetCommand/Screens/IScreen.h
+++ FleetCommand/Screens/IScreen.h
@@ -1,7 +1,18 @@
 #pragma once
 
+#include <SFML\Graphics.hpp>
+
 namespace fleet {
 	class IScreen {
+	public:
+		explicit IScreen(sf::RenderWindow& window, sf::Font& font);
+		virtual ~IScreen() = default;
 
+		virtual void input() = 0;
+		virtual void update() = 0;
+		virtual void draw() = 0;
+	private:
+		sf::RenderWindow& window;
+		sf::Font& font;
 	};
 }
--- FleetCommand/View.cpp
+++ FleetCommand/View.cpp
@@ -5,7 +5,8 @@ namespace fleet {
 		model{ model }
 	{
 		window.setFramerateLimit(60);
-		background.loadFromFile("Assets/background.png");
-		font.loadFromFile("Assets/a-song-for-jennifer/a-song-for-jennifer.ttf");
+        // Don't hard code assets. Provide pipeline to load them so that it can be flexible.
+		/*background.loadFromFile("Assets/background.png");
+		font.loadFromFile("Assets/a-song-for-jennifer/a-song-for-jennifer.ttf");*/
 	}
 }
