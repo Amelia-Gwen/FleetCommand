136628ba406f14e182f4ece70f5765714191651e
bruglesco <klitzinger@gmail.com>
2019-05-05 11:12:46 -0400

    Fixed tests for GUIButton.
    All tests now pass.

--- FleetCommandUnitTest/GUIButtonTests.cpp
+++ FleetCommandUnitTest/GUIButtonTests.cpp
@@ -36,23 +36,23 @@ namespace fleet {
 	}
 
 	TEST_F(GUIButtonTest, input) {
-		button = new GUIButton(sf::Vector2f(), "", sf::Font());
-		ASSERT_TRUE(button->input(sf::Vector2f()));
+		button = new GUIButton(sf::Vector2f(10.F, 10.F), "", sf::Font());
+		ASSERT_TRUE(button->input(sf::Vector2f(5.F, 5.F)));
 		ASSERT_FALSE(button->input(sf::Vector2f(900.F, 900.F)));
 	}
 
 	TEST_F(GUIButtonTest, button_can_be_deactivated_and_reactivated) {
-		button = new GUIButton(sf::Vector2f(), "", sf::Font());
-		ASSERT_TRUE(button->input(sf::Vector2f()));
+		button = new GUIButton(sf::Vector2f(10.F, 10.F), "", sf::Font());
+		ASSERT_TRUE(button->input(sf::Vector2f(5.F, 5.F)));
 		button->setActive(false);
-		ASSERT_FALSE(button->input(sf::Vector2f()));
+		ASSERT_FALSE(button->input(sf::Vector2f(5.F, 5.F)));
 		button->setActive(true);
-		ASSERT_TRUE(button->input(sf::Vector2f()));
+		ASSERT_TRUE(button->input(sf::Vector2f(5.F, 5.F)));
 	}
 
 	TEST_F(GUIButtonTest, update_turns_green_and_back_to_blue) {
-		button = new GUIButton(sf::Vector2f(), "", sf::Font());
-		button->update(sf::Vector2f());
+		button = new GUIButton(sf::Vector2f(10.F, 10.F), "", sf::Font());
+		button->update(sf::Vector2f(5.F, 5.F));
 		ASSERT_EQ(sf::Color::Green, button->getButton().getFillColor());
 		button->update(sf::Vector2f(900.F, 900.F));
 		ASSERT_EQ(sf::Color::Cyan, button->getButton().getFillColor());
