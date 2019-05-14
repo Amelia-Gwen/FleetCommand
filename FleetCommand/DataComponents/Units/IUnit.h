4ef6941b4d4938053040f114947dee1d94af75fa
bruglesco <klitzinger@gmail.com>
2019-05-14 15:11:57 -0400

    Created IUnit.
    IUnit is the Interface base class for all Units.

---
+++ FleetCommand/DataComponents/Units/IUnit.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+
+namespace fleet {
+
+	class IUnit {
+	public:
+		virtual ~IUnit() = default;
+
+		virtual void move() = 0;
+		virtual void fire() = 0;
+	protected:
+		unsigned short rank;
+		unsigned hitPoints;
+		unsigned short movePoints;
+		unsigned short attackPoints;
+		unsigned attack;
+		unsigned defense;
+		unsigned speed;
+		unsigned accuracy;
+		unsigned evade;
+
+		bool canBeFlagship;
+		bool isFlagship;
+	};
+}
