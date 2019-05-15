f25099612d1748f472e74faa7608b109f46a8cae
bruglesco <klitzinger@gmail.com>
2019-05-15 16:27:52 -0400

    Updated IComponent

--- FleetCommand/DataComponents/ShipComponents/IComponent.h
+++ FleetCommand/DataComponents/ShipComponents/IComponent.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <string>
+
 namespace fleet {
 	class IComponent {
 	public:
+		IComponent(const std::string& compontentType, unsigned short rank) : componentType{ componentType }, rank{ rank } {}
 		virtual ~IComponent() = default;
 	protected:
+		std::string componentType;
 		unsigned short rank;
 		unsigned cost;
 		unsigned size;
